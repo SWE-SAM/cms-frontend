115be40d384cbacd31d2bc129e1a4c9a3d8fb66c
Sam Tran <epicsanity@hotmail.com>
2026-01-03 01:36:09 +0000

    cleaned sidebar

--- remix/app/menu-items/index.js
+++ remix/app/menu-items/index.js
@@ -1,9 +1,9 @@
 import dashboard from './dashboard';
-import submitComplaint from './other';
+
 import ViewComplaintPage from './complaints';
 
 const menuItems = {
-    items: [dashboard, submitComplaint, ViewComplaintPage]
+    items: [dashboard, ViewComplaintPage]
 };
 
 export default menuItems;
